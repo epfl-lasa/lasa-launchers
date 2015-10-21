bfe482e727217404dab2d634f57cff2d70d51c68
Robot <nbfigueroa@gmail.com>
2015-10-21 13:04:49 +0200

    Pushed Launcher sub repo that is used to launch/visualize/record Kinect1+Kinect2+MoCap+KUKA Robot

---
+++ record_demo_multi_sensors/launch/dataset.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+# launching ROS
+roslaunch record_demo_multi_sensors dataset.launch session_name:=$session_name session_time:=$timestamp
+
---
+++ record_demo_multi_sensors/launch/dataset_record.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+
+# asking for session name
+echo -n "Hello! :) \nI'm getting ready..."
+# sleep 1
+echo -n "Are you already ready?...\n"
+# sleep 1
+echo -n "Good...Then: \n"
+echo -n "Please, write me the name of this session [PRESS ENTER]: \n"
+
+while [ -z "$session_name" ]
+do
+	read session_name
+	if ! [ -n "$session_name" ]; then
+		session_name="default"
+		#echo "no session recognized, enter new name:"
+	fi
+done
+sleep 1
+
+#generating session folder
+date=$( date +%F )
+time=$( date +%T )
+timestamp=$(echo "$date _ $time" | sed 's/://g' | sed 's/-//g' | sed 's/\.//g' | sed 's/\ //g')
+
+mkdir -p /home/nbfigueroa/dataset/$session_name/
+mkdir -p /home/nbfigueroa/dataset/$session_name/$timestamp
+mkdir -p /home/nbfigueroa/dataset/$session_name/$timestamp/bagfiles/
+
+# launching ROS
+roslaunch record_demo_multi_sensors dataset_bag.launch session_name:=$session_name session_time:=$timestamp
